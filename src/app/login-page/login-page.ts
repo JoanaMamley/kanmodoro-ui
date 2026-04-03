aa600738c98ad8634fb3373786d1ba9541d92521
JoanaMamley <joanateye18@gmail.com>
2026-04-03 15:22:11 +0000

    Implement login page

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'kanmodoro-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
+  protected readonly theme = inject(ThemeService);
+  private readonly fb = inject(FormBuilder);
 
+  protected showPassword = false;
+
+  protected form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  protected get f() {
+    return this.form.controls;
+  }
+
+  protected onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // TODO: call auth service
+    console.log('login', this.form.value);
+  }
 }
