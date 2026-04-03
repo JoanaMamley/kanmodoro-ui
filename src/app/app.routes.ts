7bb48e4db2d0bdcce8d3a09383ace5fc323c0845
JoanaMamley <joanateye18@gmail.com>
2026-04-03 11:40:42 +0000

    Add routes and logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { WelcomePage } from './welcome-page/welcome-page';
+import { LoginPage } from './login-page/login-page';
+import { RegisterPage } from './register-page/register-page';
+import { ForgotPassword } from './forgot-password/forgot-password';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomePage },
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'dashboard', component: Dashboard },
+  { path: '**', redirectTo: '' },
+];
