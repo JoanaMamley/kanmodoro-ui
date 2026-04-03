f94fc666b91dc4f7dfb486bf7ef5e648ae3d8674
JoanaMamley <joanateye18@gmail.com>
2026-04-03 15:21:54 +0000

    Implement registration page

--- src/app/register-page/register-page.ts
+++ src/app/register-page/register-page.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
+
+const passwordsMatchValidator: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+  const password = group.get('password')?.value;
+  const confirm = group.get('confirmPassword')?.value;
+  return password && confirm && password !== confirm ? { passwordsMismatch: true } : null;
+};
 
 @Component({
   selector: 'kanmodoro-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register-page.html',
   styleUrl: './register-page.scss',
 })
 export class RegisterPage {
+  protected readonly theme = inject(ThemeService);
+  private readonly fb = inject(FormBuilder);
 
+  protected showPassword = false;
+  protected showConfirm = false;
+
+  protected form = this.fb.group(
+    {
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: passwordsMatchValidator },
+  );
+
+  protected get f() {
+    return this.form.controls;
+  }
+
+  protected onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // TODO: call auth service
+    console.log('register', this.form.value);
+  }
 }
